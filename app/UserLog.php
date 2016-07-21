54c7ce13bc8795e5fc96a347e9b47c8992395ba6
Ariff Azraai <ariff.azraai@gmail.com>
2016-07-21 15:34:07 +0800

    Add remarks to UserLogs.

--- app/UserLog.php
+++ app/UserLog.php
@@ -6,10 +6,7 @@ class UserLog extends Model
 {
     protected $fillable = [
         'action',
-<<<<<<< e36b68adacc8101ed6edb7239c6991e135e232ec
         'remarks',
-=======
->>>>>>> Fix user log.
         'ip_address',
         'user_id'
     ];
