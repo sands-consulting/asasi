3756167d517867f1c4ca2a488dfb23ea94157a61
Ariff Azraai <ariff.azraai@gmail.com>
2016-07-21 13:27:52 +0800

    Update login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,40 @@
 @extends('layouts.window')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{trans('auth.login')}}</div>
-				<div class="panel-body">
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						{!! csrf_field() !!}
+<div class="row">
+	<div class="col-xs-12 col-sm-4 col-sm-offset-4">
+		<div class="panel panel-body login-form">
+			<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+				{!! csrf_field() !!}
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{trans('auth.email')}}</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+				<div class="text-center">
+					<div class="icon-object border-blue-800 text-blue-800"><i class="glyphicon glyphicon-off"></i></div>
+					<h5 class="content-group">{{ trans('auth.login') }}</h5>
+				</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{trans('auth.password')}}</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+				<div class="form-group has-feedback has-feedback-left">
+					<input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ trans('auth.email') }}">
+					<div class="form-control-feedback">
+						<i class="glyphicon glyphicon-user"></i>
+					</div>
+				</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> {{trans('auth.remember_me')}}
-									</label>
-								</div>
-							</div>
-						</div>
+				<div class="form-group has-feedback has-feedback-left">
+					<input type="password" class="form-control" name="password" placeholder="{{ trans('auth.password') }}">
+					<div class="form-control-feedback">
+						<i class="glyphicon glyphicon-lock"></i>
+					</div>
+				</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">{{trans('auth.login')}}</button>
+				<div class="form-group">
+					<button type="submit" class="btn bg-blue-700 btn-block legitRipple">{{trans('auth.login')}}</button>
+				</div>
 
-								<a class="btn btn-link" href="{{ url('/password/email') }}">{{trans('auth.forgot_password')}}</a>
-							</div>
-						</div>
-					</form>
+				<div class="text-center">
+					<a href="{{ url('/password/email') }}">{{trans('auth.forgot_password')}}</a>
 				</div>
 			</div>
-		</div>
+		</form>
 	</div>
 </div>
 @endsection
