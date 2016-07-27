f2fbc7e848c384066c786f814f1083fde0000cf2
Ariff Azraai <ariff.azraai@gmail.com>
2016-07-27 11:54:56 +0800

    Fix http middleware reference.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Sands\Asasi\BootedMiddleware::class,
+        \Sands\Asasi\Booted\BootedMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
