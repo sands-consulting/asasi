694f3b38d99f5e079fb1023c3fd25f479163128c
Muhammad Syafiq <syafiq@my-sands.com>
2016-09-27 16:29:50 +0800

    Invoice printing update styling.

--- resources/views/payments/printInvoice.blade.php
+++ resources/views/payments/printInvoice.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.printing')
 
 @section('content')
+<table class="container container-header">
+  <tr>
+    <td>
+      <table>
+        <tr>
+          <td class="col-9 logo-prompt">
+          </td>
+          <td class="col-3 right">
+            <h5 class="invoice-title">TAX INVOICE</h5>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
 <table class="container">
   <tr>
     <td>
@@ -18,6 +33,7 @@
             <ul class="list-condensed list-unstyled">
                 <li>Date: <span class="text-semibold">January 12, 2015</span></li>
                 <li>Due date: <span class="text-semibold">May 12, 2015</span></li>
+                <li><h5>Total Due: <span class="text-right text-semibold">RM 30.00</span></h5></li>
             </ul>
           </td>
         </tr>
@@ -30,7 +46,7 @@
     <td>
       <table>
         <tr>
-          <td class="col-8">
+          <td class="col-12">
             <span class="text-muted">Invoice To:</span>
             <ul class="list-condensed list-unstyled">
               <li><h5>Rebecca Manes</h5></li>
@@ -40,18 +56,6 @@
               <li>United Kingdom</li>
               <li>888-555-2311</li>
               <li><a href="#">rebecca@normandaxis.ltd</a></li>
-            </ul>
-          </td>
-          <td class="col-4">
-            <span class="text-muted">Payment Details:</span>
-            <ul class="list-condensed list-unstyled invoice-payment-details">
-              <li><h5>Total Due: <span class="text-right text-semibold">$8,750</span></h5></li>
-              <li>Bank name: Profit Bank Europe</li>
-              <li>Country: United Kingdom</li>
-              <li>City: London E1 8BF</li>
-              <li>Address: 3 Goodman Street</li>
-              <li>IBAN: KFH37784028476740</li>
-              <li>SWIFT code: BPT4E</li>
             </ul>
           </td>
         </tr>
@@ -65,39 +69,93 @@
       <table class="table table-lg">
           <thead>
               <tr>
-                  <th class="col-9">Description</th>
-                  <th class="col-1">Rate</th>
-                  <th class="col-1">Hours</th>
-                  <th class="col-1">Total</th>
+                  <th>Description</th>
+                  <th width="80px">GST Amount</th>
+                  <th width="80px">Amount</th>
+                  <th width="80px">Tax Code</th>
               </tr>
           </thead>
           <tbody>
               <tr>
                   <td>
-                      <h6 class="no-margin">Create UI design model</h6>
-                      <span class="text-muted">One morning, when Gregor Samsa woke from troubled.</span>
+                      <h6 class="no-margin">PPG. SEL. 10/2016 (N)</h6>
+                      <span class="text-muted">Kerja-Kerja Menaiktaraf Bengkel IAT Syarikat Ain Food Products, Pejabat Pertanian Daerah Gombak/Petaling Selangor Darul Ehsan.</span>
                   </td>
-                  <td>$70</td>
-                  <td>57</td>
-                  <td><span class="text-semibold">$3,990</span></td>
+                  <td>RM 0.00</td>
+                  <td>RM 10.00</td>
+                  <td><span class="text-semibold">N-T</span></td>
               </tr>
               <tr>
                   <td>
-                      <h6 class="no-margin">Support tickets list doesn't support commas</h6>
-                      <span class="text-muted">I'd have gone up to the boss and told him just what i think.</span>
+                      <h6 class="no-margin">PDP/PLB/55/2016</h6>
+                      <span class="text-muted">KERJA-KERJA MEMBAIKPULIH KEROSAKAN BANGUNAN DAN LAIN-LAIN KERJA BERKAITAN DI BALAI JKKK KAMPUNG BUKIT CHERAKAH DI DALAM DAERAH PETALING</span>
                   </td>
-                  <td>$70</td>
-                  <td>12</td>
-                  <td><span class="text-semibold">$840</span></td>
+                  <td>RM 0.00</td>
+                  <td>RM 10.00</td>
+                  <td><span class="text-semibold">N-T</span></td>
               </tr>
               <tr>
                   <td>
-                      <h6 class="no-margin">Fix website issues on mobile</h6>
-                      <span class="text-muted">I am so happy, my dear friend, so absorbed in the exquisite.</span>
+                      <h6 class="no-margin">PDKS/KP 200-2/1/5-1(S)</h6>
+                      <span class="text-muted">MEMBAIK PULIH DAN PEMBAIKAN TANDAS-TANDAS DISELURUH PEJABAT DAERAH/TANAH KUALA SELANGOR</span>
                   </td>
-                  <td>$70</td>
-                  <td>31</td>
-                  <td><span class="text-semibold">$2,170</span></td>
+                  <td>RM 0.00</td>
+                  <td>RM 10.00</td>
+                  <td><span class="text-semibold">N-T</span></td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td>Subtotal</td>
+                  <td><span class="text-semibold">RM 30.00</span></td>
+                  <td></td>
+              </tr>
+              <tr class="no-border">
+                  <td></td>
+                  <td>GST (6%)</td>
+                  <td><span class="text-semibold">RM 0.00</span></td>
+                  <td></td>
+              </tr>
+              <tr class="no-border">
+                  <td></td>
+                  <td>Total (incld. GST)</td>
+                  <td><span class="text-semibold">RM 30.00</span></td>
+                  <td></td>
+              </tr>
+              <tr class="no-border">
+                  <td></td>
+                  <td>Balance Due</td>
+                  <td><span class="text-semibold">RM 30.00</span></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+    </td>
+  </tr>
+</table>
+<table class="container">
+  <tr>
+    <td>
+      <table class="table table-lg">
+          <thead>
+              <tr>
+                  <th>Code</th>
+                  <th width="80px">Rate</th>
+                  <th width="80px">Tax</th>
+                  <th width="80px">Sale Amount</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>N-T</td>
+                  <td>0%</td>
+                  <td>RM 0.00</td>
+                  <td><span class="text-semibold">RM 0.00</span></td>
+              </tr>
+              <tr>
+                  <td>SR</td>
+                  <td>6%</td>
+                  <td>RM 0.00</td>
+                  <td><span class="text-semibold">RM 0.00</span></td>
               </tr>
           </tbody>
       </table>
@@ -110,32 +168,15 @@
       <table>
         <tr>
           <td class="col-8">
-            <h6>Authorized person</h6>
+            <h6>Account Details</h6>
             <ul class="list-condensed list-unstyled text-muted">
-                <li>Eugene Kopyov</li>
+                <li><span class="text-semibold">Eugene Kopyov</span></li>
                 <li>2269 Elba Lane</li>
                 <li>Paris, France</li>
                 <li>888-555-2311</li>
+                <li>IBAN: KFH37784028476740</li>
+                <li>SWIFT code: BPT4E</li>
             </ul>
-          </td>
-          <td class="col-4">
-            <h6>Total due</h6>
-            <table class="table">
-              <tbody>
-                <tr>
-                  <th>Subtotal:</th>
-                  <td class="text-right">$7,000</td>
-                </tr>
-                <tr>
-                  <th>Tax: <span class="text-regular">(25%)</span></th>
-                  <td class="text-right">$1,750</td>
-                </tr>
-                <tr>
-                  <th>Total:</th>
-                  <td class="text-right text-primary"><h5 class="text-semibold">$8,750</h5></td>
-                </tr>
-              </tbody>
-            </table>
           </td>
         </tr>
       </table>
