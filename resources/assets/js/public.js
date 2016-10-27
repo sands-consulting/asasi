50edd8b9a55c4d19623064c5e64f940bea634906
Amin Adha <amin@my-sands.com>
2016-10-28 05:13:26 +0800

    change spaces 4 to 2 for public.js

--- resources/assets/js/public.js
+++ resources/assets/js/public.js
@@ -1,19 +1,26 @@
 $(function() {
-    // Styled checkboxes, radios
-    $('.styled, .multiselect-container input').uniform({
-        radioClass: 'choice',
-        wrapperClass: 'border-primary text-primary'
-    });
-    // Styled file input
-    // $('.file-styled').uniform({
-    //     fileButtonClass: 'action btn bg-warning-400'
-    // });
-    
-    $('.daterange-single').daterangepicker({ 
-        singleDatePicker: true,
-        locale: {
-          cancelLabel: 'Clear',
-          format: 'YYYY-MM-DD'
-        }
-    }); 
+  // Styled checkboxes, radios
+  $('.styled, .multiselect-container input').uniform({
+    radioClass: 'choice',
+    wrapperClass: 'border-primary text-primary'
+  });
+  // Styled file input
+  // $('.file-styled').uniform({
+  //     fileButtonClass: 'action btn bg-warning-400'
+  // });
+  
+  $('.daterange-single').daterangepicker({ 
+    singleDatePicker: true,
+    locale: {
+      cancelLabel: 'Clear',
+      format: 'YYYY-MM-DD'
+    }
+  }); 
 });
+
+// Equal Height
+// var heights = $(".row-eq-height .media-box").map(function() {
+// return $(this).height();
+// }).get()
+// var maxHeight = Math.max.apply(null, heights);
+// $(".row-eq-height .media-box").height(maxHeight);
