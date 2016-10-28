b38c584d40f6076e039cb96efae4212ba158977a
Amin Adha <amin@my-sands.com>
2016-10-28 09:50:49 +0800

    wip - appying design to evaluation page

--- resources/views/admin/evaluations/create.blade.php
+++ resources/views/admin/evaluations/create.blade.php
@@ -13,87 +13,74 @@
 
 @section('content')
 <div class="row">
-    <div class="col-sm-4">
+    <div class="col-sm-12">
         <div class="panel panel-flat">
-            <div class="panel-heading">
-                <h6 class="panel-title">Notices</h6>
-            </div>
             <div class="panel-body">
-                <div class="row">
+                <div class="row is-table-row">
+                    <div class="col-sm-8">
+                        <div class="text-muted">{{ $notice->number }}</div>
+                        <a href="{{ route('admin.notices.show', $notice->id) }}">{{ $notice->name }}</a>
+                    </div>
+                    <div class="col-sm-2 box text-center text-muted">
+                        <div class="text-size-mini">Notice Type</div>
+                        <div>{{ $notice->type ? $notice->type->name : 'N/A' }}</div>
+                    </div>
+                    <div class="col-sm-2 box text-center text-muted">
+                        <div class="text-size-mini">Evaluation Type</div>
+                        <div>{{ $notice->type ? $notice->type->name : 'N/A' }}</div>
+                    </div>
+                </div>
+                <div class="row is-table-row">
                     <div class="col-sm-12">
-                        <div class="form-group">
-                            <label class="control-label"><strong>{{ trans('notices.attributes.name') }}</strong>:</label>
-                            <div class="form-control-static">{{ $notice->name }}</div>
-                        </div>
+                        <div class="text-muted">{{ !empty($notice->description) ? nl2br($notice->description) : 'N/A' }}</div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label class="control-label"><strong>{{ trans('notices.attributes.number') }}</strong>:</label>
-                            <div class="form-control-static">{{ $notice->number }}</div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label class="control-label"><strong>{{ trans('notices.attributes.notice_type_id') }}</strong>:</label>
-                            <div class="form-control-static">{{ $notice->type ? $notice->type->name : 'N/A' }}</div>
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label class="control-label"><strong>{{ trans('notices.attributes.description') }}</strong>:</label>
-                            <div class="form-control-static">{{ !empty($notice->description) ? nl2br($notice->description) : 'N/A' }}</div>
-                        </div>
-                    </div>
-                    {{-- <div class="col-sm-12">
-                        <div class="form-group">
-                            <label class="control-label"><strong>{{ trans('notices.attributes.rules') }}</strong>:</label>
-                            <div class="form-control-static">{!! !empty($notice->rules) ? nl2br($notice->rules) : 'N/A' !!}</div>
-                        </div>
-                    </div> --}}
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="col-sm-8">
+    <div class="col-sm-12">
         <div class="panel panel-flat">
             <div class="panel-heading">
-                <h6 class="panel-title">Evaluations</h6>
+                <h6 class="panel-title">Evaluations Criteria</h6>
             </div>
-            {!! Former::open(route('admin.evaluations.store', $notice->id)) !!}
-            <table class="table table-bordered table-striped table-condensed">
-                <thead>
-                    <tr>
-                        <th width="80px">#</th>
-                        <th>Title</th>
-                        <th width="250px">Score</th>
-                    </tr>
-                </thead>
-                <tbody>
+            <div class="panel-body">
+                {!! Former::open(route('admin.evaluations.store', $notice->id)) !!}
+
                     <?php $i = 1; ?>
                     @foreach ($evaluationRequirements as $evaluationRequirement)
-                        <tr>
-                            <td>{{ $i }}</td>
-                            <td>{{ $evaluationRequirement->title }}</td>
-                            <td>
-                                {!! Former::number('scores['. $evaluationRequirement->id .']')
+                        <div class="row is-table-row">
+                            <div class="col-sm-1">
+                                <div class="box">{{ $i }}</div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="box"> 
+                                    {{ $evaluationRequirement->title }}
+                                </div>  
+                            </div>
+                            <div class="col-sm-2 greyed">
+                                <div class="box text-center">
+                                    {!! Former::number('scores['. $evaluationRequirement->id .']')
                                     ->label(false)
                                     ->append('/ ' . $evaluationRequirement->full_score)
                                     ->addClass('text-center')
                                     ->min(0)
                                     ->max($evaluationRequirement->full_score)
-                                    ->required() !!}</td>
-                        </tr>
+                                    ->required() !!}
+                                </div>
+                            </div>
+                        </div>
                         <?php $i++; ?>
                     @endforeach
-                </tbody>
-            </table>
-            <div class="panel-footer">
-                <div class="text-right">
-                    <button type="submit" class="btn btn-primary bg-blue-400">{{ trans('actions.save') }} <i class="icon-floppy-disk position-right"></i></button>
-                </div>
+
+                    <div class="row is-table-row">
+                        <div class="col-sm-12 text center">
+                            <button type="submit" class="btn btn-primary bg-blue-400">{{ trans('actions.save') }} <i class="icon-floppy-disk position-right"></i></button>
+                        </div>
+                    </div>
+
+                {!! Former::close() !!}
             </div>
-            {!! Former::close() !!}
         </div>
     </div>
 </div>
