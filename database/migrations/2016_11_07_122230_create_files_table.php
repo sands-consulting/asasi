6292433868141771816c24e0b868e32d9d054542
Ariff Azraai <ariff.azraai@gmail.com>
2016-11-07 12:28:10 +0800

    create files table.

---
+++ database/migrations/2016_11_07_122230_create_files_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('identifier')->unique();
+            $table->string('name')->index();
+            $table->string('description');
+            $table->string('status');
+            $table->nullableTimestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('file_rules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('scope')->index();
+            $table->text('rule');
+            $table->unsignedInteger('file_id');
+            $table->nullableTimestamps();
+            $table->softDeletes();
+
+            $table->foreign('file_id')
+                ->references('id')
+                ->on('files')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('file_rules');
+        Schema::drop('files');
+    }
+}
