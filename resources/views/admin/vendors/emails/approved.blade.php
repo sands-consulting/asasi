6c4e1d0b850be0d6efcbeac4c38533fd9bf58ad0
Amin Adha <amin@my-sands.com>
2016-12-01 17:01:23 +0800

    Approved vendor notification email

---
+++ resources/views/admin/vendors/emails/approved.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Application Approved</title>
+</head>
+<body>
+    <h2>Congratulations!</h2>
+    <p>Your company {{ $vendor->name }} ({{$vendor->registration_number}}) application has been approved.
+    <br>
+    <p> Login <a href='{{ route('home') }}'>here</a> for more information. </p>
+    <br>
+    <p>Thank you.</p>
+</body>
+</html>
