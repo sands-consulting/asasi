79c84f867800545289d5959a3a094539ee5ea8ae
Amin Adha <amin@my-sands.com>
2017-01-18 00:43:58 +0800

    fixed submission

--- resources/views/admin/evaluations/_submission_actions.blade.php
+++ resources/views/admin/evaluations/_submission_actions.blade.php
@@ -1,3 +1,2 @@
-@if ($submission->status == 'completed' || $submission->status == 'submitted')
 <a href="{{ route('admin.evaluations.edit', [$submission->notice_id, $submission->submission_id]) }}">{{ trans('actions.evaluate') }}</a>
-@endif
+
