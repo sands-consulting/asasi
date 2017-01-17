2be9499612d70c030efcf051af7e9942df8a8cc1
Amin Adha <amin@my-sands.com>
2017-01-18 04:30:32 +0800

    redirect all errors to / for demo comment all not working link in notices show

--- resources/views/admin/notices/index.blade.php
+++ resources/views/admin/notices/index.blade.php
@@ -13,6 +13,13 @@
 </div>
 @endsection
 
+@section('secondary-header')
+<ul class="breadcrumb breadcrumb-caret">
+    <li><a href="{{ route('admin') }}"><i class="icon-home2 position-left"></i> Home</a></li>
+    <li class="active">{{ trans('notices.views.index.title') }}</li>
+</ul>
+@endsection
+
 @section('content')
 <div class="panel panel-flat">
     <div class="panel-body form-datatable-search form-inline">
