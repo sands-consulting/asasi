50d6061c10d7a0e64959fb160d6bfc5206c18bd6
Amin Adha <amin@my-sands.com>
2017-01-18 01:17:56 +0800

    merge evaluation requirement into notice show

--- app/Http/Controllers/Admin/EvaluationRequirementsController.php
+++ app/Http/Controllers/Admin/EvaluationRequirementsController.php
@@ -14,7 +14,7 @@ class EvaluationRequirementsController extends Controller
 {
     public function index(EvaluationNoticesDataTable $table, Notice $notice)
     {
-        return $table->render('admin.evaluation-requirements.index', compact('notice'));
+        return $table->render('admin.notices.show.evaluation-requirements', compact('notice'));
     }
 
     public function edit(Notice $notice)
@@ -22,6 +22,6 @@ class EvaluationRequirementsController extends Controller
         $requirementCommercials = $notice->evaluationRequirements()->commercials()->get();
         $requirementTechnicals = $notice->evaluationRequirements()->technicals()->get();
 
-        return view('admin.evaluation-requirements.edit', compact('notice', 'requirementCommercials', 'requirementTechnicals'));
+        return view('admin.notices.show.evaluation-requirements', compact('notice', 'requirementCommercials', 'requirementTechnicals'));
     }
 }
