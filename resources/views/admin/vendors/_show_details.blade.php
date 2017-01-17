8d8c6dd9628b40ce424bdf39e71fd440ec0f41ab
Ariff Azraai <mk9206.nx@gmail.com>
2017-01-17 16:26:24 +0800

    Update design.

--- resources/views/admin/vendors/_show_details.blade.php
+++ resources/views/admin/vendors/_show_details.blade.php
@@ -1,26 +1,34 @@
 <div class="row">
 	<div class="col-xs-12 col-md-3 col-md-push-9">
-		<div class="panel prompt-box full">
-			<h4 class="title">{{ trans('vendors.views._show_details.contact_person.title') }}</h4>
-			<p>{{ $vendor->contact_person_designation }} {{ $vendor->contact_person_name }}
-				<br>
-				<span class="text-muted">
-					<i class="icon-mail5"></i> {{ $vendor->contact_person_email }}<br>
-					<i class="icon-phone2"></i> {{ $vendor->contact_person_telephone }}
-				</span>
-			</p>
+		<div class="panel panel-flat">
+			<div class="panel-heading">
+				<h6 class="panel-title">{{ trans('vendors.views._show_details.contact_person.title') }}</h6>
+			</div>
+			<div class="panel-body">
+				<p>{{ $vendor->contact_person_designation }} {{ $vendor->contact_person_name }}
+					<br>
+					<span class="text-muted">
+						<i class="icon-mail5"></i> {{ $vendor->contact_person_email }}<br>
+						<i class="icon-phone2"></i> {{ $vendor->contact_person_telephone }}
+					</span>
+				</p>
+			</div>
 		</div>
 
-		<div class="panel prompt-box full">
-			<h4 class="title">{{ trans('vendors.views._show_details.capital.title') }}</h4>
-			<p>{{ trans('vendors.attributes.capital_authorized') }}
-				<br>
-				<span class="text-muted">{{ $vendor->capital_currency }} {{ $vendor->capital_authorized }}</span>
-			</p>
-			<p>{{ trans('vendors.attributes.capital_paid_up') }}
-				<br>
-				<span class="text-muted">{{ $vendor->capital_currency }} {{ $vendor->capital_paid_up }}</span>
-			</p>
+		<div class="panel panel-flat">
+			<div class="panel-heading">
+				<h6 class="panel-title">{{ trans('vendors.views._show_details.capital.title') }}</h6>
+			</div>
+			<div class="panel-body">
+				<p>{{ trans('vendors.attributes.capital_authorized') }}
+					<br>
+					<span class="text-muted">{{ $vendor->capital_currency }} {{ $vendor->capital_authorized }}</span>
+				</p>
+				<p>{{ trans('vendors.attributes.capital_paid_up') }}
+					<br>
+					<span class="text-muted">{{ $vendor->capital_currency }} {{ $vendor->capital_paid_up }}</span>
+				</p>
+			</div>
 		</div>
 	</div>
 
