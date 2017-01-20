c19487afe9fc87189a95eb1203c313d812a0dc65
Ariff Azraai <mk9206.nx@gmail.com>
2017-01-20 20:32:41 +0800

    Remove repositories.

--- app/Repositories/RolesRepository.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-class RolesRepository extends BaseRepository
-{
-}
--- app/Repositories/UserBlacklistsRepository.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-class UserBlacklistsRepository extends BaseRepository
-{
-}
