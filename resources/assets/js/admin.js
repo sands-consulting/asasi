b0997f0e6eefc3ad1dc37863e26a3f997b4e3ab8
Amin Adha <amin@my-sands.com>
2017-03-08 16:21:38 +0800

    fixed graph error

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -136,15 +136,3 @@ var heights = $(".row-eq-height .eq-element").map(function() {
 }).get()
 var maxHeight = Math.max.apply(null, heights);
 $(".row-eq-height .eq-element").height(maxHeight);
-
-// Javascript to enable link to tab
-// var url = document.location.toString();
-// if (url.match('#')) {
-//     $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-// } 
-
-// // Change hash for page-reload
-// $('.nav-tabs a').on('shown.bs.tab', function (e) {
-//     e.preventDefault();
-//     window.location.hash = e.target.hash;
-// })
