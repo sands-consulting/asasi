696d3e6211386e4071a081e0615cf3129d735c7c
Ariff Azraai <mk9206.nx@gmail.com>
2017-04-07 18:16:50 +0800

    Update page pending.

--- resources/views/vendors/pending.blade.php
+++ resources/views/vendors/pending.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts.portal')
 
 @section('content')
-<div class="page-container">
-    <div class="page-content">
-        <div class="content-wrapper">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12">
-                    <div class="panel panel-white">
-                        <div class="panel-heading">
-                            <h4 class="panel-title pull-left">{{ trans('vendors.views.pending.title') }}</h4>
-                            {!! link_to_route('root', trans('vendors.views.pending.back'), [], ['class' => 'btn bg-blue-700 pull-right legitRipple']) !!}
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="panel-body">
-                            {!! trans('vendors.views.pending.content', ['vendor-name' => $vendor->name]) !!}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+@include('layouts.portal.widgets.wizard')
+
+<div class="panel panel-white">
+    <div class="panel-heading">
+        <h4 class="panel-title pull-left">{{ trans('vendors.views.pending.title') }}</h4>
+        {!! link_to_route('root', trans('vendors.views.pending.back'), [], ['class' => 'btn bg-blue-700 pull-right legitRipple']) !!}
+        <div class="clearfix"></div>
+    </div>
+    <div class="panel-body">
+        {!! trans('vendors.views.pending.content', ['vendor-name' => $vendor->name]) !!}
     </div>
 </div>
 @stop
