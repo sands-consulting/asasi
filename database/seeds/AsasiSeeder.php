3f8e85c0b0ad4a8ffbf716d6371eaf85072708ff
Amin Adha <amin@my-sands.com>
2017-04-14 12:31:53 +0800

    updated placeseeder

--- database/seeds/AsasiSeeder.php
+++ database/seeds/AsasiSeeder.php
@@ -58,41 +58,6 @@ class AsasiSeeder extends Seeder
             'short_name' => 'ACME'
         ]);
 
-        $places = [
-            ['Malaysia',        'MY',   'MAS',  'country',  null],     #1
-
-            ['Johor',           null,   'JHR',  'state', 1],    #2
-            ['Kedah',           null,   'KDH',  'state', 1],    #3
-            ['Kelantan',        null,   'KEL',  'state', 1],    #4
-            ['WP Kuala Lumpur', null,   'KUL',  'state', 1],    #5
-            ['WP Labuan',       null,   'LBN',  'state', 1],    #6
-            ['Melaka',          null,   'MEL',  'state', 1],    #7
-            ['Negeri Sembilan', null,   'NEG',  'state', 1],    #8
-            ['Pahang',          null,   'PHG',  'state', 1],    #9
-            ['Pulau Pinang',    null,   'PNG',  'state', 1],    #10
-            ['WP Putrajaya',    null,   'PUJ',  'state', 1],    #11
-            ['Perak',           null,   'PRK',  'state', 1],    #12
-            ['Perlis',          null,   'PER',  'state', 1],    #13
-            ['Sabah',           null,   'SBH',  'state', 1],    #14
-            ['Sarawak',         null,   'SWK',  'state', 1],    #15
-            ['Selangor',        null,   'SEL',  'state', 1],    #16
-            ['Terengganu',      null,   'TRG',  'state', 1],    #17
-
-            ['Kuala Lumpur',    null,   null,   'city', 5],
-            ['Shah Alam',       null,   null,   'city', 16]
-        ];
-
-        foreach($places as $place)
-        {
-            PlaceService::create(new Place(), [
-                'name'      => $place[0],
-                'code_2'    => $place[1],
-                'code_3'    => $place[2],
-                'type'      => $place[3],
-                'parent_id' => $place[4]
-            ]);
-        }
-
         $taxes = [
             [
                 'name' => 'Standard Rated',
