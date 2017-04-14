b22a447e0bf19acd508f88d3ecae95382fef8c13
Amin Adha <amin@my-sands.com>
2017-04-14 13:11:13 +0800

    fixed news form validation

--- app/Http/Requests/NewsRequest.php
+++ app/Http/Requests/NewsRequest.php
@@ -11,10 +11,22 @@ class NewsRequest extends FormRequest
         return true;
     }
 
-    public function rules()
+    public function storeRules()
     {
         return [
-            'title'             => 'required',
+            'title'             => 'required|max:255',
+            'summary'           => 'required|max:255',
+            'content'           => 'required',
+            'category_id'       => 'required|exists:news_categories,id',
+            'organization_id'   => 'exists:organizations,id'
+        ];
+    }
+
+    public function updateRules()
+    {
+        return [
+            'title'             => 'required|max:255',
+            'summary'           => 'required|max:255',
             'content'           => 'required',
             'category_id'       => 'required|exists:news_categories,id',
             'organization_id'   => 'exists:organizations,id'
