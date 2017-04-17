9498846ae6a8f7a75f92fb5cd2a4e2b40c9becc7
Amin Adha <amin@my-sands.com>
2017-04-17 09:41:32 +0800

    fixed dropdown on award form

--- resources/views/admin/notices/show/award.blade.php
+++ resources/views/admin/notices/show/award.blade.php
@@ -2,13 +2,17 @@
 	{!! Former::open(route('admin.notices.award', $notice->id))->novalidate()->addClass('panel panel-flat') !!}
 		{{ Former::populate($notice->award) }}
 		<div class="panel-body">
-			{!! Former::select('submission_id')->label('notices.views.admin.show.award.submission')->options($notice->submissions()->whereStatus('submitted')->get())->placeholder('notices.views.admin.show.award.select-submission')->required() !!}
+            {!! Former::select('submission_id')
+                ->label('notices.views.admin.show.award.submission')
+                ->options($notice->submissions()->whereStatus('submitted')->pluck('number', 'id'))
+                ->placeholder('notices.views.admin.show.award.select-submission')
+                ->required() !!}
 			{!! Former::text('period')->required()->placeholder('notices.views.admin.show.award.period') !!}
 			{!! Former::text('price')->required()->placeholder('notices.views.admin.show.award.price') !!}
 			{!! Former::select('status_award')->label('notices.attributes.status')->options(collect(trans('statuses'))->only('published', 'pending'))->required() !!}
 		</div>
 		<div class="panel-footer">
-			<input type="submit" value="{{ trans('actions.save') }}"" class="btn bg-blue-700 pull-right">
+            <input type="submit" value="{{ trans('actions.save') }}" class="btn bg-blue-700 pull-right">
 		</div>
 	{!! Former::close() !!}
 </div>
